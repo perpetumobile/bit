e4de7d519e7fc936dbd18f5de18d7dfef02e7234
zdukic <zdukic@gmail.com>
2014-07-02 11:39:03 -0700

    test serialization

--- test/com/perpetumobile/bit/orm/json/JSONParserManagerTest.java
+++ test/com/perpetumobile/bit/orm/json/JSONParserManagerTest.java
@@ -1,12 +1,18 @@
 package com.perpetumobile.bit.orm.json;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 
 public class JSONParserManagerTest extends TestCase {
+	
+	public static final String JSON_DATA_FILE_NAME = "json.data";
 	
 	public static TestSuite suite() {
 		TestSuite suite = new TestSuite();
@@ -26,6 +32,33 @@ public class JSONParserManagerTest extends TestCase {
 	}
 	
 	protected void tearDown() {
+		File file = new File(JSON_DATA_FILE_NAME);
+		if(file.exists()) {
+			file.delete();
+		}
+	}
+	
+	protected void write(JSONRecord rec, String fileName) {
+		try {			
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName, false));
+			out.writeObject(rec);
+			out.flush();
+			out.close();
+		} catch (Exception e) {
+			fail("Exception at JSONParserManagerTest.write: " + e.getLocalizedMessage());
+		}
+	}
+	
+	protected JSONRecord read(String fileName) {
+		JSONRecord result = null;
+		try {			
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(fileName));
+			result = (JSONRecord)in.readObject();
+			in.close();
+		} catch (Exception e) {
+			fail("Exception at JSONParserManagerTest.write: " + e.getLocalizedMessage());
+		}
+		return result;
 	}
 	
 	public void test1() {
@@ -35,7 +68,13 @@ public class JSONParserManagerTest extends TestCase {
 		try {
 			JSONRecord result = JSONParserManager.getInstance().parseImpl(new File("data/json/sample_1.txt"), "JSONSample1");
 			StringBuilder buf = result.generateJSON(true);
-			System.out.println(buf.toString());
+			String jsonStr = buf.toString();
+			System.out.println(jsonStr);
+			// test serialization
+			write(result, JSON_DATA_FILE_NAME);
+			result = read(JSON_DATA_FILE_NAME);
+			buf = result.generateJSON(true);
+			assertEquals(jsonStr, buf.toString());
 		} catch (Exception e) {
 			fail("Exception at JSONParserManagerTest.test1: " + e.getLocalizedMessage());
 		}
@@ -48,7 +87,13 @@ public class JSONParserManagerTest extends TestCase {
 		try {
 			JSONRecord result = JSONParserManager.getInstance().parseImpl(new File("data/json/sample_2.txt"), "JSONSample2");
 			StringBuilder buf = result.generateJSON(true);
-			System.out.println(buf.toString());
+			String jsonStr = buf.toString();
+			System.out.println(jsonStr);
+			// test serialization
+			write(result, JSON_DATA_FILE_NAME);
+			result = read(JSON_DATA_FILE_NAME);
+			buf = result.generateJSON(true);
+			assertEquals(jsonStr, buf.toString());
 		} catch (Exception e) {
 			fail("Exception at JSONParserManagerTest.test2: " + e.getLocalizedMessage());
 		}
